314a6b8d42db2c84d27133926c175a2d905fb493
Mariotti7 <midiasmart99@gmail.com>
2020-11-09 14:55:48 -0300

    GET & POST blogPessoal

--- blogPessoal/blogPessoal/src/app/app.module.ts
+++ blogPessoal/blogPessoal/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HttpClientModule } from '@angular/common/http';
+import { OrderModule } from 'ngx-order-pipe';
+import { PostTemaComponent } from './post-tema/post-tema.component';
 
 
 @NgModule({
@@ -24,14 +26,16 @@ import { HttpClientModule } from '@angular/common/http';
     FeedComponent,
     PerfilLateralComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    PostTemaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blogPessoal/blogPessoal/src/app/post-tema/post-tema.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-post-tema',
+  templateUrl: './post-tema.component.html',
+  styleUrls: ['./post-tema.component.css']
+})
+export class PostTemaComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
+  constructor(
+    private temaService: TemaService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    this.finAllTemas()
+  }
+
+  finAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=>{
+      this.listaTemas= resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.tema.id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  cadastrar(){
+    if(this.tema.descricao == null ){
+      alert('Preencha o Campo!')
+    }else{
+      this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+        this.tema = resp
+        this.router.navigate(['/feed'])
+        alert('Tema Cadastrado Com Sucesso')
+      })
+    }
+  }
+
+}
